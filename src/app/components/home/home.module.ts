143d737ba2804d4ab8e8f076d8857e04e95cae92
kaianamiguel <kaiana.miguel@gmail.com>
2023-11-21 22:19:30 -0300

    fix: changing home

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from '../home/home.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { BtnPrimaryComponent } from '../../shared/btn-primary/btn-primary.component';
-import { MainSectionComponent } from 'src/app/shared/main-section/main-section.component';
+import { MainMeComponent } from 'src/app/shared/main-me/main-me.component';
 import { MainAnimationComponent } from 'src/app/shared/main-animation/main-animation.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MainAboutComponent } from 'src/app/shared/main-about/main-about.component';
+import { MainProjectsComponent } from 'src/app/shared/main-projects/main-projects.component';
+import { ProjectComponent } from 'src/app/shared/project/project.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     BtnPrimaryComponent,
-    MainSectionComponent,
-    MainAnimationComponent
+    MainMeComponent,
+    MainAnimationComponent,
+    MainAboutComponent,
+    MainProjectsComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
